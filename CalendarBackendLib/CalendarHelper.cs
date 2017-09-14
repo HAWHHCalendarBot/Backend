25933518237a884a76f893cde59d49f571b99348
Edgar Toll <edjopato@gmail.com>
2017-09-14 23:37:55 +0200

    refactor: move weekday stuff to a generalized class

---
+++ CalendarBackendLib/CalendarHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace CalendarBackendLib
+{
+    public class CalendarHelper
+    {
+        public static DateTime DateOfWeekDayISO8601(int year, int week, DayOfWeek weekday)
+        {
+            var firstDayOfWeek = FirstDateOfWeekISO8601(year, week);
+            var weekdayNumber = (int)weekday - 1; // Monday is the first day, so it has to be 0
+            return firstDayOfWeek.AddDays(weekdayNumber);
+        }
+
+        // https://stackoverflow.com/questions/662379/calculate-date-from-week-number
+        // yaay, works with the week 54+ stuff ootb
+        public static DateTime FirstDateOfWeekISO8601(int year, int weekOfYear)
+        {
+            DateTime jan1 = new DateTime(year, 1, 1);
+            int daysOffset = DayOfWeek.Thursday - jan1.DayOfWeek;
+
+            DateTime firstThursday = jan1.AddDays(daysOffset);
+            var cal = CultureInfo.CurrentCulture.Calendar;
+            int firstWeek = cal.GetWeekOfYear(firstThursday, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+
+            var weekNum = weekOfYear;
+            if (firstWeek <= 1)
+            {
+                weekNum -= 1;
+            }
+            var result = firstThursday.AddDays(weekNum * 7);
+            return result.AddDays(-3);
+        }
+    }
+}
--- Downloader/InformatikTxtContentParser.cs
+++ Downloader/InformatikTxtContentParser.cs
@@ -1,7 +1,6 @@
 ﻿using CalendarBackendLib;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -19,7 +18,7 @@ namespace Downloader
             new Regex("Name,Dozent,Raum,Tag,Anfang,Ende")
         };
 
-        private static readonly string[] WEEKDAYS = { "Mo", "Di", "Mi", "Do", "Fr", "Sa", "So" };
+        private static readonly string[] WEEKDAYS = { "So", "Mo", "Di", "Mi", "Do", "Fr", "Sa" };
 
         public static EventEntry[] GetEvents(string informatikTxtContent)
         {
@@ -102,40 +101,14 @@ namespace Downloader
             var startTime = TimeSpan.Parse(match.Groups[5].Value);
             var endTime = TimeSpan.Parse(match.Groups[6].Value);
 
-            var date = GetDateFromEventInfos(year, week, weekday);
+            var dayOfWeek = (DayOfWeek)Array.IndexOf(WEEKDAYS, weekday);
+            var date = CalendarHelper.DateOfWeekDayISO8601(year, week, dayOfWeek);
             var start = date.Add(startTime);
             var end = date.Add(endTime);
 
             var desc = string.IsNullOrWhiteSpace(dozent) ? "" : "Prof: " + dozent;
 
             return new EventEntry(name, start, end, room, desc);
-        }
-
-        private static DateTime GetDateFromEventInfos(int year, int week, string weekday)
-        {
-            var firstDayOfWeek = FirstDateOfWeekISO8601(year, week);
-            var weekdayNumber = Array.IndexOf(WEEKDAYS, weekday);
-            return firstDayOfWeek.AddDays(weekdayNumber);
-        }
-
-        // https://stackoverflow.com/questions/662379/calculate-date-from-week-number
-        // yaay, works with the week 54+ stuff ootb
-        private static DateTime FirstDateOfWeekISO8601(int year, int weekOfYear)
-        {
-            DateTime jan1 = new DateTime(year, 1, 1);
-            int daysOffset = DayOfWeek.Thursday - jan1.DayOfWeek;
-
-            DateTime firstThursday = jan1.AddDays(daysOffset);
-            var cal = CultureInfo.CurrentCulture.Calendar;
-            int firstWeek = cal.GetWeekOfYear(firstThursday, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
-
-            var weekNum = weekOfYear;
-            if (firstWeek <= 1)
-            {
-                weekNum -= 1;
-            }
-            var result = firstThursday.AddDays(weekNum * 7);
-            return result.AddDays(-3);
         }
     }
 }
