44d5e693b11eacd25423240864abf14d3c60690d
Edgar Toll <edjopato@gmail.com>
2017-09-15 02:32:06 +0200

    chore(mensa): add dockerfile and container to build

--- build-docker-containers.sh
+++ build-docker-containers.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 docker build -t hawhhcalendardownloader -f Dockerfile.Downloader .
+docker build -t hawhhcalendarmensa -f Dockerfile.Mensa .
 docker build -t hawhhcalendarparser -f Dockerfile.Parser .
