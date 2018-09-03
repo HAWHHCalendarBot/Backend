b1216791fb73041271b9a8a54a86e1c4d2c69bbe
Edgar To <edjopato@gmail.com>
2018-09-03 18:53:22 +0200

    chore(docker): push to hawhhcalendarbot
    dont use edjopato for that

--- docker-push.sh
+++ docker-push.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 function tag {
-  docker tag $1 edjopato/$1:latest
-  docker push edjopato/$1:latest
+  docker tag hawhhcalendarbot$1 hawhhcalendarbot/$1:latest
+  docker push hawhhcalendarbot/$1:latest
 }
 
-tag hawhhcalendarbotdownloader
-tag hawhhcalendarbotmensa
-tag hawhhcalendarbotparser
+tag downloader
+tag mensa
+tag parser
