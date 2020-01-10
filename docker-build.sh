c3001a17f44c1b4cc777a13d53e365ba50eba5af
EdJoPaTo <edjopato@gmail.com>
2020-01-10 14:43:58 +0100

    feat(Mensa): remove Mensa
    was replaced by mensa-crawler
    https://github.com/HAWHHCalendarBot/mensa-crawler

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 docker build -t hawhhcalendarbotdownloader -f Dockerfile.Downloader .
-docker build -t hawhhcalendarbotmensa -f Dockerfile.Mensa .
 docker build -t hawhhcalendarbotparser -f Dockerfile.Parser .
