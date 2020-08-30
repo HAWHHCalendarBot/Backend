3c8b232d91394078eb19a4da36818a6fbcf4b2d3
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-08-30 21:32:53 +0200

    chore(dockerhub): use image tags of dockerhub
    also include version 1 as they will probably replaced by rust as v2

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-docker build -t hawhhcalendarbotdownloader -f Dockerfile.Downloader .
-docker build -t hawhhcalendarbotparser -f Dockerfile.Parser .
+docker build -t hawhhcalendarbot/downloader:1 -f Dockerfile.Downloader .
+docker build -t hawhhcalendarbot/parser:1 -f Dockerfile.Parser .
